a2eb5db67ef7897b4e8610e12afe9ba21ca54f54
featuredspace39 <113187498+featuredspace39@users.noreply.github.com>
2024-02-21 13:39:15 -0500

    Revert "hi matteo"
    This reverts commit 142e2a3ea8abce5110e8096e309ddf7a9fc823b8.

--- firstGithubProject/src/Matteo_Poole.java
+++ firstGithubProject/src/Matteo_Poole.java
@@ -2,9 +2,5 @@ public class Matteo_Poole {
     public static void main(String[]args){
         System.out.println("Hello");
         System.out.println("Hello Other Editor Dhruvp");
-
-        System.out.println("Hello Matteo- rohan");
-        System.out.println("Hello Editor Dhruv- rohan");
-        System.out.println("Hello Editor Jeremiah- rohan");
     }
 }
